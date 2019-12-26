7baefb059a69e47dc4257439eb204d7fb874bcb9
ryan way <ryan.keith.way@gmail.com>
2019-12-26 14:08:44 -0800

    starting cell view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CellComponent } from './cell/cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule,
