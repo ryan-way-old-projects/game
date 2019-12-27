d3946908b57589a092ccaf0f658ae06d89d1ec03
ryan way <ryan.keith.way@gmail.com>
2019-12-26 16:21:36 -0800

    implement subgrid for sudoku game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CellComponent } from './cell/cell.component';
+import { SubgridComponent } from './subgrid/subgrid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CellComponent
+    CellComponent,
+    SubgridComponent
   ],
   imports: [
     BrowserModule,
--- src/app/cell/cell.component.ts
+++ src/app/cell/cell.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cell',
+  selector: 'cell',
   templateUrl: './cell.component.html',
   styleUrls: ['./cell.component.css']
 })
