c6cc638910dc95e750f1c1057294fc2b68399947
Ryan Way <ryan.keith.way@gmail.com>
2020-08-24 21:36:37 -0700

    theming, enable hot reloading (again)

--- main.js
+++ main.js
@@ -2,19 +2,37 @@ const {app, BrowserWindow} = require('electron')
 const url = require('url')
 const path = require('path')
 
-let win
+let win;
+
+const args = process.argv.slice(1),
+    serve = args.some(val => val === '--serve');
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600})
-  win.loadURL(
-    url.format({
-      pathname: path.join(__dirname, `/dist/game/index.html`),
-      protocol: "file:",
-      slashes: true
-    })
-  );
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: '#000000'
+  })
+  if (serve) {
+    require('electron-reload')(__dirname, {
+      electron: require(`${__dirname}/node_modules/electron`)
+    });
+    win.loadURL('http://localhost:4200');
+  } else {
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, `/dist/game/index.html`),
+        protocol: "file:",
+        slashes: true
+      })
+    );
+  }
   console.log(path.join(__dirname, `/dist/game/index.html`))
 
 }
 
 app.on('ready', createWindow)
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
