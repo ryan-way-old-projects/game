8244adeb4c68b87000690d4b39470a01e069d0d8
Ryan Way <ryan.keith.way@gmail.com>
2020-09-23 22:03:38 -0700

    closes #15: clearing test data

--- src/app/sudoku/sudoku.component.ts
+++ src/app/sudoku/sudoku.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sudoku',
@@ -7,26 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SudokuComponent implements OnInit {
 
-  Subgrids: {Cells: {Content: number, Selected: boolean, Highlighted: boolean}[]}[]
+  @Input() Subgrids: {Cells: {Content: number, Selected: boolean, Highlighted: boolean}[]}[]
 
   constructor() { }
 
   ngOnInit() {
-    this.Subgrids = new Array<{Cells: {Content: number, Selected: boolean, Highlighted: boolean}[]}>(9).fill(
-      {
-        "Cells": [
-          { "Content" : 1, "Selected" : false, "Highlighted" : false},
-          { "Content" : 2, "Selected" : false, "Highlighted" : true},
-          { "Content" : 3, "Selected" : true,  "Highlighted" : false},
-          { "Content" : 4, "Selected" : true,  "Highlighted" : true},
-          { "Content" : 5, "Selected" : false, "Highlighted" : false},
-          { "Content" : 6, "Selected" : false, "Highlighted" : true},
-          { "Content" : 7, "Selected" : true,  "Highlighted" : false},
-          { "Content" : 8, "Selected" : true,  "Highlighted" : true},
-          { "Content" : 9, "Selected" : false, "Highlighted" : false},
-        ]
-      }
-    )
+    //this.Subgrids = new Array<{Cells: {Content: number, Selected: boolean, Highlighted: boolean}[]}>(9).fill(
+    //  {
+    //    "Cells": [
+    //      { "Content" : 1, "Selected" : false, "Highlighted" : false},
+    //      { "Content" : 2, "Selected" : false, "Highlighted" : true},
+    //      { "Content" : 3, "Selected" : true,  "Highlighted" : false},
+    //      { "Content" : 4, "Selected" : true,  "Highlighted" : true},
+    //      { "Content" : 5, "Selected" : false, "Highlighted" : false},
+    //      { "Content" : 6, "Selected" : false, "Highlighted" : true},
+    //      { "Content" : 7, "Selected" : true,  "Highlighted" : false},
+    //      { "Content" : 8, "Selected" : true,  "Highlighted" : true},
+    //      { "Content" : 9, "Selected" : false, "Highlighted" : false},
+    //    ]
+    //  }
+    //)
   }
 
 }
