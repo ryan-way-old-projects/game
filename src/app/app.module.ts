081b262986514aae0408d7b7451072a7d664bdfc
Ryan Way <ryan.keith.way@gmail.com>
2021-04-26 10:12:55 -0700

    adding sudokue game skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SudokuGameComponent } from './sudokugame/sudokugame.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SudokuGameComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sudokugame/sudokugame.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sudokugame',
+  templateUrl: './sudokugame.component.html',
+  styleUrls: ['./sudokugame.component.css']
+})
+export class SudokuGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
