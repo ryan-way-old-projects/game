743b78b3acb1edfb52d2561734b9f47126e2feff
Ryan Way <ryan.keith.way@gmail.com>
2021-04-26 09:26:38 -0700

    GAM-11 purge old code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CellViewComponent } from './cell-view/cell-view.component';
-import { SubgridComponent } from './subgrid/subgrid.component';
-import { SudokuComponent } from './sudoku/sudoku.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CellViewComponent,
-    SubgridComponent,
-    SudokuComponent
   ],
   imports: [
     BrowserModule,
