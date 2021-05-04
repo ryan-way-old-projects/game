6f762ebf08d620061fc03f35a19a83713db405f8
Ryan Way <ryan.keith.way@gmail.com>
2021-05-03 20:49:22 -0700

    Mocked up sudokue cell with highlighting and selection styles, working on circle style

--- src/app/sudokucell/sudokucell.component.spec.ts
+++ src/app/sudokucell/sudokucell.component.spec.ts
@@ -5,6 +5,7 @@ import { SudokuCellComponent } from './sudokucell.component';
 describe('SudokuCellComponent', () => {
   let component: SudokuCellComponent;
   let fixture: ComponentFixture<SudokuCellComponent>;
+  let input: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,9 +18,20 @@ describe('SudokuCellComponent', () => {
     fixture = TestBed.createComponent(SudokuCellComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    input = fixture.debugElement.nativeElement.querySelector('input');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('onFocus should fire OnFocus event', () => {
+    let focused: boolean = false;
+    component.OnFocus.subscribe((event: string) => {
+      focused = true;
+    });
+    component.focus();
+    expect(focused).toBeTruthy();
+  });
+
 });
--- src/app/sudokucell/sudokucell.component.ts
+++ src/app/sudokucell/sudokucell.component.ts
@@ -9,7 +9,10 @@ export class SudokuCellComponent implements OnInit {
 
   @Input() Value: number;
   @Input() ReadOnly: boolean;
+  @Input() Selected: boolean;
+  @Input() Highlighted: boolean;
 
+  @Output() OnClick: EventEmitter<string> = new EventEmitter();
   @Output() OnFocus: EventEmitter<string> = new EventEmitter();
   @Output() OnBlur: EventEmitter<string> = new EventEmitter();
 
@@ -18,12 +21,22 @@ export class SudokuCellComponent implements OnInit {
   ngOnInit() {
   }
 
-  onFocus() {
+  focus() {
     this.OnFocus.emit('Focused');
   }
 
-  onBlur() {
+  blur() {
+    console.log("blurred");
+    this.Selected = true;
+    this.Highlighted = false;
     this.OnBlur.emit('Burred');
+  }
+
+  click() {
+    console.log("Clicked");
+    this.Selected = false;
+    this.Highlighted = true;
+    this.OnClick.emit('Clicked');
   }
 
 }
