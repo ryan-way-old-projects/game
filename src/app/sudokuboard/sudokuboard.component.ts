b7a1ddbd208cd9eb68eeee5630661de7fbc717c0
Ryan Way <ryan.keith.way@gmail.com>
2021-05-04 08:04:03 -0700

    SudokuBoard: added skeleton

---
+++ src/app/sudokuboard/sudokuboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sudokuboard',
+  templateUrl: './sudokuboard.component.html',
+  styleUrls: ['./sudokuboard.component.css']
+})
+
+export class SudokuBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
