63607df85e694189f360812ea9e53ea101808ead
Ryan Way <ryan.keith.way@gmail.com>
2021-05-11 08:21:41 -0700

    Simple Sudoku Board vm

---
+++ src/app/sudokuboardvm.spec.ts
@@ -0,0 +1,7 @@
+import { SudokuBoardVm } from './sudokuboardvm';
+
+describe('SudokuBoardVm', () => {
+  it('should create an instance', () => {
+    expect(new SudokuBoardVm()).toBeTruthy();
+  });
+});
