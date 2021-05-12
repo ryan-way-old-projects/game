db06a74a4ffee94ff929ebf93424aa3709b39830
Ryan Way <ryan.keith.way@gmail.com>
2021-05-12 08:02:06 -0700

    finished upgrading packages; material grid list test works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
