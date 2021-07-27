4c3df8a180498a14372d02c3b0b395af88c548bf
Ryan Way <ryan.keith.way@gmail.com>
2021-07-26 20:18:14 -0700

    changing to single quotes

--- src/app/sudokucellvm.spec.ts
+++ src/app/sudokucellvm.spec.ts
@@ -2,6 +2,6 @@ import { SudokuCellVm } from './sudokucellvm';
 
 describe('SudokuCellVm', () => {
   it('should create an instance', () => {
-    expect(new SudokuCellVm("")).toBeTruthy();
+    expect(new SudokuCellVm('')).toBeTruthy();
   });
 });
